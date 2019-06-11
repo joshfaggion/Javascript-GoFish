a8a691801c3dbcd171dec6e0d0dca1892f1747ae
Joshua Faggion <joshua.faggion@rolemodelsoftware.com>
2019-06-10 20:14:58 -0400

    Created Deck object with full testing support

--- spec/DeckSpec.js
+++ spec/DeckSpec.js
@@ -1,6 +1,29 @@
 describe('Deck', () => {
   it('can have 52 cards', () => {
     const deck = new Deck()
-    expect(deck.cardAmount).toEqual(52)
+    expect(deck.cardAmount()).toEqual(52)
+  });
+
+  it('can shuffle the cards', () => {
+    const deck = new Deck()
+    const cards = [...deck.cards()]
+    deck.shuffle()
+    expect(deck.cards()).not.toEqual(cards)
+  });
+
+  it('can take the top card', () => {
+    const deck = new Deck()
+    card = deck.topCard()
+    expect(deck.cardAmount()).toEqual(51)
+    // constructor.name gives the class name of whatever it is called on
+    expect(card.constructor.name).toEqual('Card')
+  });
+
+  it('can deal five cards to a player ', () => {
+    const deck = new Deck()
+    const dealtCards = deck.dealHand()
+    // Deal hand returns the top five cards
+    expect(deck.cardAmount()).toEqual(47)
+    expect(dealtCards.length).toEqual(5)
   });
 });
--- spec/GameViewSpec.js
+++ spec/GameViewSpec.js
@@ -1,12 +1,12 @@
-describe('GameView', () => {
-  describe('on startGame', () => {
-    // it('will place the player\'s name on the screen', () => {
-    //   const view = new GameView();
-    //   const container = document.createElement('div')
-    //   document.body.appendChild(container)
-    //   view.draw(container)
-    //   // Check for the players title
-    //   expect(view.playerTitle).toEqual("Joshua")
-    });
-  });
-});
+// describe('GameView', () => {
+//   describe('on startGame', () => {
+//     it('will place the player\'s name on the screen', () => {
+//       const view = new GameView();
+//       const container = document.createElement('div')
+//       document.body.appendChild(container)
+//       view.draw(container)
+//       // Check for the players title
+//       expect(view.playerTitle).toEqual("Joshua")
+//     });
+//   });
+// });
